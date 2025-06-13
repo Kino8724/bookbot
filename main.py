de09b9bb69e5910e2cfe014f752ef938966b1cfa
Kino8724 <ptakwyatt@gmail.com>
2025-06-12 18:40:05 -0700

    Completed project lol

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-from stats import get_word_count
+from stats import get_word_count, get_character_count, dictionary_to_sorted_list
+import sys
 
 
 def get_book_text(file_path):
-    with open(file_path) as f:
-        file_contents = f.read()
-        return file_contents
+    try:
+        with open(file_path) as f:
+            file_contents = f.read()
+            return file_contents
+    except FileNotFoundError:
+        print("Error: File not found.")
 
 
 def main():
-    book_text = get_book_text('./books/frankenstein.txt')
-    word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the document")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        book_text = get_book_text(book_path)
+        word_count = get_word_count(book_text)
+        character_count_dictionary = get_character_count(book_text)
+        character_count_sorted_list = dictionary_to_sorted_list(
+            character_count_dictionary)
+        print(f"""============ BOOKBOT ============
+    Analyzing book found at {book_path}...
+    ----------- Word Count ----------
+    Found {word_count} total words
+    --------- Character Count -------""")
+        for dict in character_count_sorted_list:
+            if dict["char"].isalpha():
+                print(f'{dict["char"]}: {dict["num"]}')
+        print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,40 @@ def get_word_count(text_from_book):
     words = text_from_book.split()
     word_count = len(words)
     return word_count
+
+
+def get_character_count(text_from_book):
+    # Convert all text to lowercase
+    lowercase_text = text_from_book.lower()
+
+    characters = {}
+
+    # Loop through each character in text
+    for char in lowercase_text:
+        # Check if character is a key in the dictionary
+        # If character is a key already increment the count value
+        if char in characters:
+            characters[char] += 1
+        # If not a key, create a key for that character, set value to 1
+        else:
+            characters[char] = 1
+    return characters
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def dictionary_to_sorted_list(dictionary):
+    list_of_dictionaries = []
+    # Loop through the big dictionary
+    for key in dictionary:
+        # Create a new dictionary for each key value pair
+        new_dict = {}
+        new_dict["char"], new_dict["num"] = key, dictionary[key]
+        # Append the new dict to the list
+        list_of_dictionaries.append(new_dict)
+
+    # Sort the list of dictionaries by value from big to small
+    list_of_dictionaries.sort(key=sort_on, reverse=True)
+    return list_of_dictionaries
